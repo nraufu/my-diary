3be8d9cd5d059d2da502424227b9eeb3a91d65ec
nraufu <nraufu@gmail.com>
2019-10-24 21:45:36 +0200

    ft(PUT entry): /PUT modify an entry
    -create /PUT endpoint
    -create testing case
    
    [finihses #169349051]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { check } from 'express-validator/check';
+import { check } from 'express-validator';
 import entry from '../controllers/entryController';
 
-const router =express.Router();
+const router = express.Router();
 
- 
+
 /* GET API base */
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.json({
         myDiary: 'API v1'
     })
@@ -19,6 +19,31 @@ router.get('/entries', entry.getAllEntries);
 router.get('/entries/:id', entry.getEntry);
 
 /* POST a new entry */
-router.post('/entries/', [check('timestamp').isInt(), check('title').isString(), check('content').isString()], entry.addEntry);
+router.post('/entries/', [check('timestamp')
+.isInt(), 
+check('title').isString(), 
+check('content').
+isString()], 
+entry.addEntry);
 
+/* PUT new a data in existing entry */
+router.put(
+    '/entries/:id',
+    [
+        check('timestamp')
+        .not()
+        .exists()
+        .withMessage('Timestamp cannot be modified'),
+        check('title')
+        .isString()
+        .optional(),
+        check('content')
+        .isString()
+        .optional(),
+        check('isFavorite')
+        .isBoolean()
+        .optional(),
+    ],
+    entry.modifyEntry,
+);
 module.exports = router;
