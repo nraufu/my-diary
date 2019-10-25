a3b95c8542b33e1de25a212721a6fb8b8ffcb4c2
nraufu <nraufu@gmail.com>
2019-10-25 22:35:27 +0200

    ch(heroku): host app on heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,13 @@ import entry from '../controllers/entryController';
 const router = express.Router();
 
 
+/* GET API base */
+router.get('/', (req, res) => {
+    res.send({
+        myDiary: 'API v1'
+    })
+});
+
 /* GET all user entries */
 router.get('/entries', entry.getAllEntries);
 
